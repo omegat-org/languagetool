baa4ac8b7339e9a6568f4ad84566d9c499d5455c
Hiroshi Miura <miurahr@linux.com>
2026-05-04 17:58:50 +0900

    chore: add conditional signing configuration using in-memory PGP keys or GPG command

--- build.gradle.kts
+++ build.gradle.kts
@@ -34,6 +34,26 @@ nexusPublishing.repositories {
     }
 }
 
+val signKey = listOf("signingKey", "signing.keyId", "signing.gnupg.keyName").find {project.hasProperty(it)}
+tasks.withType<Sign> {
+    onlyIf { signKey != null }
+}
+
+signing {
+    when (signKey) {
+        "signingKey" -> {
+            val signingKey: String? by project
+            val signingPassword: String? by project
+            useInMemoryPgpKeys(signingKey, signingPassword)
+        }
+        "signing.keyId" -> {
+        }
+        "signing.gnupg.keyName" -> {
+            useGpgCmd()
+        }
+    }
+}
+
 val wholeProjects by configurations.creating
 
 repositories {
