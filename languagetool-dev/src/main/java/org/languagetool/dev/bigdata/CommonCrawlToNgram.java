45d0a6e26e4b8a4f4f94930c90a638b36a40c338
Hiroshi Miura <miurahr@linux.com>
2026-05-21 20:12:38 +0900

    chore: transition to `numericValue` for field parsing and remove unused `LongPoint` field
    - Replace `parseLong` with `numericValue` for consistent and modern handling of numeric fields.
    - Remove unused `LongPoint` addition in `CommonCrawlToNgram`.
    - Refactor `StartTokenCounter` and related classes for better readability and maintainability.

--- languagetool-dev/src/main/java/org/languagetool/dev/bigdata/CommonCrawlToNgram.java
+++ languagetool-dev/src/main/java/org/languagetool/dev/bigdata/CommonCrawlToNgram.java
@@ -189,7 +189,7 @@ class CommonCrawlToNgram implements AutoCloseable {
       } else if (topDocs.totalHits.value == 1) {
         int docNumber = topDocs.scoreDocs[0].doc;
         Document document = index.reader.document(docNumber);
-        long oldCount = Long.parseLong(document.getField("count").stringValue());
+        long oldCount = document.getField("count").numericValue().longValue();
         //System.out.println(ngram + " -> " + oldCount + "+" + entry.getValue());
         index.indexWriter.deleteDocuments(ngram);
         index.indexWriter.addDocument(getDoc(entry.getKey(), oldCount + entry.getValue()));
@@ -216,13 +216,8 @@ class CommonCrawlToNgram implements AutoCloseable {
   private Document getDoc(String ngram, long count) {
     Document doc = new Document();
     doc.add(new Field("ngram", ngram, StringField.TYPE_NOT_STORED));
-    doc.add(getCountField(count));
+    doc.add(new StoredField("count", count));
     return doc;
-  }
-
-  @NotNull
-  private LongPoint getCountField(long count) {
-    return new LongPoint("count", count);
   }
 
   private void addTotalTokenCountDoc(long totalTokenCount, IndexWriter writer) throws IOException {
